3c3b422d2888228ea4e6d1f6a34968e0773e507f
Sudarshan Senthilvel <sudarshandesigns@gmail.com>
2018-11-06 06:58:41 +0530

    00001

---
+++ BookOne/1.Swift_Apprentice_v3.0/Ch01/B01Ch01.playground/Contents.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+// Challenge 1
+
+let exercises: Int = 9
+var exercisesSolved: Int = 0
+
+exercisesSolved += 1
+
+// Challenge 2
+
+var age: Int = 16
+print(age)
+age = 30
+print(age)
+
+exercisesSolved += 1
+
+// Challenge 3
+
+let a: Int = 46
+let b: Int = 10
+
+let answer1: Int = (a * 100) + b
+let answer2: Int = (a * 100) + (b * 100)
+let answer3: Int = (a * 100) + (b / 10)
+
+exercisesSolved += 1
+
+// Challenge 4
+
+var ans1 = 5 * 3 - 4 / 2 * 2
+var ans2 = (5 * 3) - ((4 / 2) * 2)
+
+exercisesSolved += 1
+
+// challenge 5
+
+let a1: Double = 5
+let b1: Double = 10
+let average: Double = (a1 + b1) / 2
+
+exercisesSolved += 1
+
+
